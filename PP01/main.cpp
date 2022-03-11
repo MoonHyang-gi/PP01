06777ba149b552b775179be4de429a71e46c8b21
uhbbang33 <55686424+uhbbang33@users.noreply.github.com>
2022-03-11 16:51:18 +0900

    [HG] 첫 커밋

---
+++ PP01/main.cpp
@@ -0,0 +1,19 @@
+﻿#include <stdio.h>
+#include <iostream>
+
+//using namespace std //std)standard library
+
+//namespace VR
+//{
+//    int cout;
+
+//    int main()
+//    {
+//        cout = 1;
+//        //C스타일
+//        printf("Choi Won babo");
+//        //C++
+//        std::cout << "Kim babo";
+//        return 0;
+//    }
+//}
